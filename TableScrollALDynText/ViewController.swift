aec7d8b661340dc93a2cc30fcf7c39885ddf7b3c
aaronvegh <aaron@innoveghtive.com>
2014-08-14 16:21:45 -0400

    Initial Commit

---
+++ TableScrollALDynText/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableScrollALDynText
+//
+//  Created by Aaron Vegh on 2014-08-14.
+//  Copyright (c) 2014 Aaron Vegh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
